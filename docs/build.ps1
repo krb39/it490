2692332e6cff309309c5ff15aa93b20255b6dfbb
Ryan Tolboom <rxt1077@nnjit.edu>
2020-04-24 16:16:32 -0400

    Continued work on documentation

--- docs/build.ps1
+++ docs/build.ps1
@@ -3,4 +3,6 @@ $adoc = "$rubybin\asciidoctor.bat"
 $epub = "$rubybin\asciidoctor-epub3.bat"
 
 & $adoc -r asciidoctor-diagram -D output index.adoc
-& $epub -r asciidoctor-diagram -D output index.adoc
+& $epub -r asciidoctor-diagram -D output -o systems-integration.epub index.adoc
+& $epub -r asciidoctor-diagram -D output -a ebook-format=kf8 -o systems-integration.mobi index.adoc
+Remove-Item -Path .\output\systems-integration-kf8.epub
